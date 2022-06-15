6a6b5101d0751dc0208901235239661ce3aa1d04
Ali Dandan <alidandan17@outlook.com>
2022-06-15 16:07:21 +0300

    functionality and methods galore

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -16,3 +17,45 @@ class EstatePropertyOffer(models.Model):
     property_type_id = fields.Many2one('estate.property.type',
                                        related="property_id.property_type_id",
                                        string='Property Type')
+    validity = fields.Integer(string='Validity', default=7)
+    date_deadline = fields.Date(string='Deadline', compute="_compute_deadline",
+                                inverse="_inverse_deadline")
+
+    def action_accept(self):
+        for record in self:
+            record.status = 'accepted'
+            record.property_id.state = 'sold'
+            record.property_id.buyer_id = record.partner_id
+            record.property_id.selling_price = record.price
+        return True
+
+    def action_refuse(self):
+        for record in self:
+            record.status = 'refused'
+            record.property_id.state = 'offer recieved'
+            record.property_id.buyer_id = False
+            record.property_id.selling_price = False
+        return True
+
+    @api.depends("validity", "date_deadline")
+    def _compute_deadline(self):
+        for record in self:
+            if record.validity:
+                if record.validity > 0:
+                    record.date_deadline = fields.Date.today() + \
+                        relativedelta(days=+record.validity)
+                else:
+                    record.status = 'refused'
+                    record.validity = 0
+                    break
+
+    def _inverse_deadline(self):
+        for record in self:
+            if record.validity:
+                if record.validity > 0:
+                    record.date_deadline = fields.Date.today() + \
+                        relativedelta(days=+record.validity)
+                else:
+                    record.status = 'refused'
+                    record.validity = 0
+                    break
--- models/estate_property_tag.py
+++ models/estate_property_tag.py
@@ -7,5 +7,5 @@ class EstatePropertyTag(models.Model):
 
     name = fields.Char(required=True)
     color = fields.Integer(string='color')
-    property_ids = fields.Many2many('estate.properties','property_tag_id',
+    property_ids = fields.Many2many('estate.properties', 'property_tag_id',
                                     string='Properties')
